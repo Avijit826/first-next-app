5d8757b8668b0d2a34e832b4c43ec7004dde5cff
Avijit826 <avijitdas826@gmail.com>
2023-10-02 18:48:05 +0530

    Initial Commit

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+const Home = () => {
+  return (
+    <section className='w-full flex-col flex-center'>
+      <h1 className='text-center head_text'>
+        Collaborate & Share
+        <br className='max-md:hidden' />
+        <span className='orange_gradient text-center'>
+          Your Notes with Ease
+        </span>
+      </h1>
+      <p className='desc text-center'>
+        The Ultimate Note Sharing App for All Your Needs. Whether You Want to
+        Create Notes with Rich Media, Organize Them in Different Ways, Sync Them
+        Across Devices, Collaborate with Others, or Share Them with the World,
+        Note It Has You Covered.
+      </p>
+    </section>
+  )
+}
+
+export default Home
